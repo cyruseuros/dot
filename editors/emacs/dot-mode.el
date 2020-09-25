0c2a2a540e050ae2ede54d8ab0a7dc3231e33461
jjzmajic <uros.m.perisic@gmail.com>
2020-09-25 10:30:49 +0800

    Move minimal dot-mode to language repo.

---
+++ editors/emacs/dot-mode.el
@@ -0,0 +1,31 @@
+;;; snippets/dot-mode.el -*- lexical-binding: t; -*-
+
+(defvar dot-mode-symbol-regexp
+  "[[:lower:]-]+"
+  "Intentionally strict symbol definition for `dot-mode'")
+
+(defvar dot-mode-font-lock-keywords
+  `(,(concat "-" dot-mode-symbol-regexp) font-lock-builtin-face)
+  "`dot-mode' keywords to be used in `font-lock-defaults'.")
+
+(defvar dot-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    (modify-syntax-entry ?\' "\"" table) ; strings
+    table)
+  "Syntax table used in `dot-mode'.")
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.dot\\'" . dot-mode))
+;;;###autoload
+(add-to-list 'interpreter-mode-alist '("dot" . dot-mode))
+;;;###autoload
+(define-derived-mode dot-mode prog-mode ".dot"
+  "A major mode for the .dot programming language."
+  :syntax-table dot-mode-syntax-table
+  (setq-local font-lock-defaults
+              `(,dot-mode-font-lock-keywords
+                nil t nil nil
+                ;; TODO: add fancy functions to detect function bodies etc.
+                )))
+
+(provide 'dot-mode)
